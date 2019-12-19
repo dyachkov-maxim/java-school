d45ef1ff1a6f31591a130ff41b4299eaec9616d9
Your Name <you@example.com>
2019-12-19 15:44:40 +0300

    Added homework16

---
+++ homework16/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map <Integer, String> map = new HashMap<>();
+        for (int i = 0; i < 100_000; i++) {
+            map.put(i, String.format("value %d", i));
+        }
+    }
+}
